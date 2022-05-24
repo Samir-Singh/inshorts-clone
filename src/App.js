82ed971cdf8171c5c571a27ab3946a29f93a57cc
Samir Singh <41338872+Samir-Singh@users.noreply.github.com>
2022-05-24 12:26:17 +0530

    "api change"

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
   const newsApi = async () =>{
     try{
       // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-      const news = await axios.get(`https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`);
-      // const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}&pageSize=${loadmore}`)
+      // const news = await axios.get(`https://saurav.tech/NewsAPI/top-headlines/category/${category}/in.json`);
+      const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${apiKey}&pageSize=${loadmore}`)
       setNewsArray(news.data.articles);
       setNewsResults(news.data.totalResults);
       // console.log(news.data.articles);
